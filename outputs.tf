a238cec3fe25d4a7736fa27aec3cacec9fb2fa2d
piyush <piyush@piyush-Lenovo-IdeaPad-S340-14IWLsharmapiyush.gmail.com>
2025-09-18 14:30:54 +0530

    w

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
-output "ec2_instance_id" { value=aws_instance.web.id }
-output "ec2_public_ip"   { value=aws_instance.web.public_ip }
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
 
 output "rds_endpoint" {
-  value = var.use_rds ? aws_db_instance.mydb[0].endpoint : "No RDS in Dev"
+  value = var.use_rds ? aws_db_instance.mydb[0].endpoint : "Local MySQL on EC2"
 }
--- vpc.tf
+++ vpc.tf
@@ -29,7 +29,10 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block="0.0.0.0/0"; gateway_id=aws_internet_gateway.igw.id }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = merge(local.common_tags, { Name = "${var.env_name}-public-rt" })
 }
 
@@ -41,7 +44,7 @@ resource "aws_route_table_association" "public_assoc" {
 
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.main.id
-  name = "${var.env_name}-sg"
+  name   = "${var.env_name}-sg"
 
   dynamic "ingress" {
     for_each = local.ingress_rules
@@ -54,7 +57,10 @@ resource "aws_security_group" "sg" {
   }
 
   egress {
-    from_port=0; to_port=0; protocol="-1"; cidr_blocks=["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(local.common_tags, { Name = "${var.env_name}-sg" })
